b7ecc3e1b56e8166d8bfddbfc14db5ec83a06c12
toxigeno <melissaaraya@yahoo.com>
2021-02-14 18:42:47 +0000

    6.4 hasta

--- exercises/06.2-Loop-from-the-top/app.js
+++ exercises/06.2-Loop-from-the-top/app.js
@@ -1,6 +1,6 @@
 var mySampleArray = [3423, 5, 4, 47889, 654, 8, 867543, 23, 48, 56432, 55, 23, 25, 12];
 
 for (var i = mySampleArray.length - 1; i >= 0; i--) {
-    reverseMe = mySampleArray[i];
-    console.log(reverseMe);
+    console.log(mySampleArray[i]); 
 };
+
