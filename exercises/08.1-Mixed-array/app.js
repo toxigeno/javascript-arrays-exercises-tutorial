d7d829b0f94b4e181465ac524e013ce6f4d70475
toxigeno <melissaaraya@yahoo.com>
2021-02-18 05:11:49 +0000

    hasta 8.1 hecho en clase

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,9 @@
-var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
+var mix = [42, true, "towel", [2, 1], 'hello', 34.4, { "name": "juan" }];
 
 //your code here
+let itemType = [];
+for (let i = 0; i < mix.length; i++) {
+    itemType.push(typeof (mix[i]));
+};
+
+console.log(itemType);
