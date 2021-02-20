56e94df49919712d2f87aa66cbb6215d09d1e85e
toxigeno <melissaaraya@yahoo.com>
2021-02-20 06:13:40 +0000

    hasta .3 resuelto

--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -1,8 +1,12 @@
-function sumTheElements(theArray){
-	
-	var total = 0;
-	
-	//your code here
+let theArray = [2,13,34,5];
+let otroArray = [12,3,37,5];
 
+function sumTheElements(arr){
+    var total = 0;
+    for (let i = 0; i < arr.length; i++) {
+         total = arr[i] + total;
+    }
 	return total;
 }
+let suma1 = sumTheElements(theArray);
+let suma2 = sumTheElements(otroArray);
