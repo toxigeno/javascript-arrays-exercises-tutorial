0d07d65c42ff9127ff2cb89c0d8663aa418d850e
toxigeno <melissaaraya@yahoo.com>
2021-02-21 05:57:56 +0000

    nada de nada

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,17 @@
 let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
 
-// your code here
+let even = [];
+let odd = [];
+
+mergeTwoList.forEach(function(a, b){
+    if (a % 2){
+        even.push(a);
+    }
+    if(b != a){
+        odd.push(b);
+    }
+});
 
 
 console.log(mergeTwoList(list_of_numbers))
+
--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,1 +1,5 @@
 var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+
+let aux = [0];
+const biggestNumber = myArray.filter(MyArray => MyArray >= 21);
+console.log(biggestNumber);
